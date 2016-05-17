e9629262e272fd48599037ad87bf552f5b9c8ffb
pjotar <sviridov.1987@gmail.com>
2016-05-18 00:37:01 +0300

    мелие исправления

--- src/main/java/ru/plastikam/mail/configuration/Config.java
+++ src/main/java/ru/plastikam/mail/configuration/Config.java
@@ -4,8 +4,9 @@ public class Config {
 
     public static class Mail {
 
-        public static final long scheduleDelayMs = 60000;
-        public static final long maxMailPerRound = 1;
+        public static final long scheduleDelayMs = 5000;
+
+        public static final long maxMailPerRound = 5;
 
         public static class In {
 
@@ -17,9 +18,13 @@ public class Config {
         }
 
         public static class Out {
-            public static final String username = "Recover@plastikam-team.ru";
 
-            public static final String password = "slonslon";
+            public static final String username = "conf5843@gmail.com";
+
+            public static final String password = "321qazxc";
+
+//            public static final String username = "Recover@plastikam-team.ru";
+//            public static final String password = "slonslon";
 
             public static final String recipient = "roistat.plastikam@gmail.com";
         }
